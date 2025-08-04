cac4816d1ea679cdf0d3af9f74217f0b6125b95d
Bharathimula <bharathim273@gmail.com>
2025-08-04 19:12:12 +0530

    modified

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,18 +5,15 @@ from .models import CustomUser
 class SignupForm(UserCreationForm):
     password1 = forms.CharField(
         label="Password",
-        widget=forms.PasswordInput
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'})
     )
     password2 = forms.CharField(
         label="Confirm Password",
-        widget=forms.PasswordInput
+        widget=forms.PasswordInput(attrs={'placeholder': 'Re-enter password'})
     )
 
     class Meta:
         model = CustomUser
-        fields = [
-            'first_name', 'last_name', 'username', 'email', 'user_type',
-            'profile_picture', 'address_line1', 'city', 'state', 'pincode',
-            'password1', 'password2'
-        ]
+        fields = ['first_name', 'last_name', 'username', 'email', 'user_type',
+                  'profile_picture', 'address_line1', 'city', 'state', 'pincode', 'password1', 'password2']
 
