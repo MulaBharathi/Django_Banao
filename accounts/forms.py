a1ba1ca37513bd2054f70abe43c9284d9828abea
Bharathimula <bharathim273@gmail.com>
2025-08-04 19:07:25 +0530

    Modified

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,12 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class SignupForm(UserCreationForm):
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput
+    )
 
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'username', 'email', 'user_type',
-                  'profile_picture', 'address_line1', 'city', 'state', 'pincode', 'password1', 'password2']
-
+        fields = [
+            'first_name', 'last_name', 'username', 'email', 'user_type',
+            'profile_picture', 'address_line1', 'city', 'state', 'pincode',
+            'password1', 'password2'
+        ]
 
