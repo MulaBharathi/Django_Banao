4be4459fedc84509d94d9ba8a20f81e7eca65d79
Bharathimula <bharathim273@gmail.com>
2025-08-04 18:53:16 +0530

    Modified forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,11 @@ from .models import CustomUser
 
 class SignupForm(UserCreationForm):
     password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name', 'username', 'email', 'user_type',
-                  'profile_picture', 'address_line1', 'city', 'state', 'pincode', 'password1', 'password2']
+                  'profile_picture', 'address_line1', 'city', 'state', 'pincode', 'password1', 'confirm_password']
+
 
