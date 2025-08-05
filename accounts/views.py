901e5fd6328696253a8a0144946ad788c42907e2
Bharathimula <bharathim273@gmail.com>
2025-08-05 07:51:48 +0530

    Modified

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .forms import SignupForm
 from .models import CustomUser
